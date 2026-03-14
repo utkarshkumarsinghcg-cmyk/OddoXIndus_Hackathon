90cc5bcc7a86b6ff405f3ae5a6a7a816a1f0e56a
priyabratasahoo780 <jisik82797@indevgo.com>
2026-03-14 18:05:20 +0530

    Add Render deployment configuration and update pool.js

--- frontend_1/vite.config.js
+++ frontend_1/vite.config.js
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000', // TODO: Change this to your local backend URL and port
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
+  plugins: [react()],
 })
