d1009882834a549e4c2a650b5afe51864f428f02
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-03-14 12:20:03 +0530

    chore: add .gitignore and remove node_modules from tracking

--- backend_old/src/utils/otpGenerator.js
+++
@@ -1,7 +0,0 @@
-const generateOTP = () => {
-  // Generate a 6 digit random number
-  const otp = Math.floor(100000 + Math.random() * 900000);
-  return otp.toString();
-};
-
-module.exports = { generateOTP };
