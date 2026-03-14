4baa8f29983aa761fdbea3bae9a6a0b3fae1fe8c
RonitkumarSoni <ronitkumarsoni.cg@gmail.com>
2026-03-14 15:02:21 +0530

    feat: push all local backend source and config files

--- Backend2/src/routes/authRoutes.js
+++ Backend2/src/routes/authRoutes.js
@@ -6,15 +6,12 @@ const {
   forgotPassword,
   verifyOTP,
   resetPassword,
-  verifyPhoneOTP,
 } = require('../controllers/authController');
-const { protect } = require('../middleware/authMiddleware');
 
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/forgot-password', forgotPassword);
 router.post('/verify-otp', verifyOTP);
 router.post('/reset-password', resetPassword);
-router.post('/verify-phone-otp', protect, verifyPhoneOTP);
 
 module.exports = router;
